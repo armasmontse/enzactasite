61f881f7dfe642ec9ce9e7986b3cb8b7e9949f92
armasmontse <montserrat.armasg@gmail.com>
2026-09-21 12:41:15 -0600

    WENZ-1670 | KO | SO | Add October business days notice

--- Customer/iboblast/KORIBO/KR/NoticeBoard.php
+++ Customer/iboblast/KORIBO/KR/NoticeBoard.php
@@ -117,6 +117,20 @@ hr {
     <div class="col_1 alleft bo-ibo-title Notice-4rd-COL">게시일:</div>
     <div class="clear"></div>
 
+    <!--  Business Days in October -->
+    <div class="col_3 mleft Notice-1st-COL">
+        <span class="green bold p12">&nbsp; &nbsp; &nbsp;162</span>
+    </div>
+    <div class="col_3 alleft p10 Notice-2nd-COL">10월 영업 안내</div>
+    <div class="col_1 alcenter Notice-3rd-COL">
+        <a target='_blank' href="https://media.enzactainternational.com/iboblast/docs/KR/Notice/BusinessOctober_KR_260921.png">
+            <img src="https://enzactamedia.enzacta.com/prod/Customer/iboblast/MXIBO/SP/images/downloadarrow-icon.svg" width="20" height="20" alt="Down" />
+        </a>
+    </div>
+    <div class= "col_3 alleft p10 Notice-4rd-COL">9/21/2026</div>
+    <div class="clear"></div>
+    <hr />
+
     <!--  Shipment Delay Notice -->
     <div class="col_3 mleft Notice-1st-COL">
         <span class="green bold p12">&nbsp; &nbsp; &nbsp;161</span>
